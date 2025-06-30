95f6e67099955c032c5c18d86dace7eba148b6e5
Florian Zunke <florianzunke@gmx.de>
2025-06-30 18:58:51 +0200

    fix product serializer

--- market_app/api/serializers.py
+++ market_app/api/serializers.py
@@ -38,16 +38,13 @@ class SellerSerializer(serializers.ModelSerializer):
     
 
 
-
-
-
 class ProductDetailSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=255)
     description = serializers.CharField()
     price = serializers.DecimalField(max_digits=50, decimal_places=2)
-    market = serializers.StringRelatedField(many=True)
-    seller = serializers.StringRelatedField(many=True)
+    market = MarketSerializer(write_only=True)
+    seller = SellerSerializer(write_only=True)
 
 
 
@@ -55,65 +52,23 @@ class ProductCreateSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=255)
     description = serializers.CharField()
     price = serializers.DecimalField(max_digits=50, decimal_places=2)
-    market = serializers.ListField(child=serializers.IntegerField(), write_only=True)
-    seller = serializers.ListField(child=serializers.IntegerField(), write_only=True)
+    market = serializers.IntegerField(write_only=True)
+    seller = serializers.IntegerField(write_only=True)
 
-    def validate_markets(self, value):
-        market = Market.objects.filter(id__in=value)
-        if len(market) != len(value):
-            raise serializers.ValidationError({"messsage": "passt halt nicht mit den IDs"})
+    def validate_market(self, value):
+        if not Market.objects.filter(id=value).exists():
+            raise serializers.ValidationError("Market with this ID does not exist.")
         return value
 
-    def validate_sellers(self, value):
-        seller = Seller.objects.filter(id_in=value)
-        if len(seller) != len(value):
-            raise serializers.ValidationError({"messsage": "passt halt nicht mit den IDs"})
+    def validate_seller(self, value):
+        if not Seller.objects.filter(id=value).exists():
+            raise serializers.ValidationError("Seller with this ID does not exist.")
         return value
-
 
     def create(self, validated_data):
-        market_id= validated_data.pop('market')
-        seller_id= validated_data.pop('seller')
-        product = Product.objects.create(**validated_data)
-        markets_list = Market.objects.filter(id__in=market_id)
-        sellers_list = Seller.objects.filter(id__in=seller_id)
-        product.market.set(markets_list)
-        product.seller.set(sellers_list)
+        market_id = validated_data.pop('market')
+        seller_id = validated_data.pop('seller')
+        market = Market.objects.get(id=market_id)
+        seller = Seller.objects.get(id=seller_id)
+        product = Product.objects.create(market=market, seller=seller, **validated_data)
         return product
-
-# {
-# "name" : "Eis",
-# "description" : "was kaltes zum essen",
-# "price" : "3.99",
-# "market" : [1],
-# "seller" : [1]
-# }
-
-
-
-
-# class SellersDetailSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     name = serializers.CharField(max_length=255)
-#     contact_info = serializers.CharField()
-#     # markets = MarketSerializer(many=True, read_only=True)
-#     markets = serializers.StringRelatedField(many=True)
-
-
-# class SellersCreateSerializer(serializers.Serializer):
-#     name = serializers.CharField(max_length=255)
-#     contact_info = serializers.CharField()
-#     markets = serializers.ListField(child=serializers.IntegerField(), write_only=True)
-
-#     def validate_markets(self, value):
-#         markets = Market.objects.filter(id__in=value)
-#         if len(markets) != len(value):
-#             raise serializers.ValidationError({"messsage": "passt halt nicht mit den IDs"})
-#         return value
-
-#     def create(self, validated_data):
-#         market_id= validated_data.pop('markets')
-#         seller = Seller.objects.create(**validated_data)
-#         markets_list = Market.objects.filter(id__in=market_id)
-#         seller.markets.set(markets_list)
-#         return seller
